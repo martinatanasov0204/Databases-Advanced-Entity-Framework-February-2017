b3d28d84f18ca6c37413406ef8ea95916cf01280
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-01 22:25:37 +0200

    Add files via upload

---
+++ 02-exercise-introduction-to-entity-framework/03-employees-full-information/Startup.cs
@@ -0,0 +1,29 @@
+﻿namespace _03_employees_full_information
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            var softUniContext = new SoftUniContext();
+
+            //Filter employees
+
+            var employees = softUniContext.Employees
+                .OrderBy(e => e.EmployeeID)
+                .Select(e => new { e.FirstName, e.MiddleName, e.LastName, e.JobTitle, e.Salary})
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", 
+                                    employee.FirstName, 
+                                    employee.LastName, 
+                                    employee.MiddleName, 
+                                    employee.JobTitle, 
+                                    employee.Salary);
+            }
+        }
+    }
+}
