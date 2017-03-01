babbd30488e8e0cc347175d042b3a2225dcdfb8d
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-01 22:34:08 +0200

    Add files via upload

---
+++ 02-exercise-introduction-to-entity-framework/12-increase-salaries/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace _12_increase_salaries
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            var softUniContext = new SoftUniContext();
+
+            //Filter employees
+
+            var employees = softUniContext.Employees
+                .Where(e => e.Departments1.Name == "Engineering"
+                            || e.Departments1.Name == "Tool Design"
+                            || e.Departments1.Name == "Marketing"
+                            || e.Departments1.Name == "Information Services");
+
+            foreach (var employee in employees)
+            {
+                //Increase salaries by 12% and print results
+
+                var currSalary = employee.Salary;
+                employee.Salary = currSalary * (decimal)(0.12) + currSalary;
+
+                Console.WriteLine("{0} {1} (${2}) ", employee.FirstName, employee.LastName, employee.Salary);
+            }
+            
+            //Save changes
+
+            softUniContext.SaveChanges();
+        }
+    }
+}
