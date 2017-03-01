ee32f1c2226ae6e5097a74826e89a57f1fe0b875
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-01 22:31:58 +0200

    Add files via upload

---
+++ 02-exercise-introduction-to-entity-framework/10-departments-with-more-than-5-employees/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace _10_departments_with_more_than_5_employees
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            var softUniContext = new SoftUniContext();
+
+            //Filter departments
+
+            var departments = softUniContext.Departments
+                .Where(d => d.Employees1.Count > 5)
+                .OrderBy(e => e.Employees1.Count)
+                .ToList();
+
+            foreach (var department in departments)
+            {
+                var managerName = department.Employees.FirstName;
+
+                Console.WriteLine("{0} {1}", department.Name, managerName);
+
+                //Print all employees from each department
+
+                foreach (var employee in department.Employees1)
+                {
+                    Console.WriteLine("{0} {1} {2}", employee.FirstName, employee.LastName, employee.JobTitle);
+                }
+            }
+        }
+    }
+}
