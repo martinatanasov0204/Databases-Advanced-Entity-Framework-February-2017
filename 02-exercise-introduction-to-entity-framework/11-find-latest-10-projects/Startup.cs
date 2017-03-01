e2dd65eef8ea6509525ed054bc3992bb93e27aab
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-01 22:32:50 +0200

    Add files via upload

---
+++ 02-exercise-introduction-to-entity-framework/11-find-latest-10-projects/Startup.cs
@@ -0,0 +1,34 @@
+﻿namespace _11_find_latest_10_projects
+{
+    using System.Threading;
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+            var softUniContext = new SoftUniContext();
+
+            //Filter projects
+
+            var projects = softUniContext.Projects
+                .OrderByDescending(p => p.StartDate)
+                .Take(10)
+                .OrderBy(p => p.Name)
+                .ToList();
+
+            foreach (var project in projects)
+            {
+                //Print each project details
+
+                Console.WriteLine("{0} {1} {2:M/d/yyyy h:mm:ss tt} {3:M/d/yyyy h:mm:ss tt}", 
+                    project.Name, 
+                    project.Description, 
+                    project.StartDate,
+                    project.EndDate);
+            }
+        }
+    }
+}
