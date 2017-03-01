417b765abaf0ee73e8bc40f2524f33d9b40df38d
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-01 22:30:31 +0200

    Add files via upload

---
+++ 02-exercise-introduction-to-entity-framework/08-addresses-by-town-name/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace _08_addresses_by_town_name
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            var softUniContext = new SoftUniContext();
+
+            //Filter addresses
+
+            var addresses = softUniContext.Addresses
+                .OrderByDescending(e => e.Employees.Count)
+                .ThenBy(e => e.Towns.Name)
+                .Take(10)
+                .ToList();
+
+            foreach (var address in addresses)
+            {
+                Console.WriteLine("{0}, {1} - {2} employees", address.AddressText, address.Towns.Name, address.Employees.Count);
+            }
+        }
+    }
+}
