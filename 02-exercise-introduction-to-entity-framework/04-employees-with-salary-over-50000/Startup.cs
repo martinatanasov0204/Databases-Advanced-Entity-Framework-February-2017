d68f6480f1af6a39eb31424b1c4cc158ed916b29
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-01 22:27:00 +0200

    Add files via upload

---
+++ 02-exercise-introduction-to-entity-framework/04-employees-with-salary-over-50000/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace _04_employees_with_salary_over_50000
+{
+    using System;
+    using System.Linq;
+
+    class Startup
+    {
+        static void Main()
+        {
+            var softUniContext = new SoftUniContext();
+
+            //Filer employees
+
+            var employees = softUniContext.Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => e.FirstName)
+                .ToArray();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("{0}", employee);
+            }
+        }
+    }
+}
