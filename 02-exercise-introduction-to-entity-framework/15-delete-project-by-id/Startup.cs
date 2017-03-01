9e8e331918bcd5ecf5762c7dc15089e317fe0370
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-01 22:36:19 +0200

    Add files via upload

---
+++ 02-exercise-introduction-to-entity-framework/15-delete-project-by-id/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace _15_delete_project_by_id
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            var softUniContext = new SoftUniContext();
+
+            //Find project with Id - 2
+
+            var project = softUniContext.Projects.Find(2);
+
+            //Delete project from employees who reference it
+
+            project.Employees.Clear();
+
+            //Remove the project from database
+
+            softUniContext.Projects.Remove(project);
+
+            softUniContext.SaveChanges();
+
+            var projectsToPrint = softUniContext.Projects
+                .Select(p => p.Name)
+                .Take(10);
+
+            //Print projects names
+
+            foreach (var item in projectsToPrint)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
