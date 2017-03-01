78f682e7202c9a6aae5b27e78168822a95c05b8b
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-01 22:31:13 +0200

    Add files via upload

---
+++ 02-exercise-introduction-to-entity-framework/09-employee-with-id-147/Startup.cs
@@ -0,0 +1,30 @@
+﻿namespace _09_employee_with_id_147
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            var softUniContext = new SoftUniContext();
+
+            //Get the employee with Id - 147
+
+            var employee = softUniContext.Employees
+                .Find(147);
+
+            var projects = employee.Projects
+                .OrderBy(p => p.Name)
+                .ToList();
+
+            Console.WriteLine("{0} {1} {2} ", employee.FirstName, employee.LastName, employee.JobTitle);
+
+            //Print all project names from this employee
+
+            foreach (var project in projects)
+            {
+                Console.WriteLine(project.Name);
+            }
+        }
+    }
+}
