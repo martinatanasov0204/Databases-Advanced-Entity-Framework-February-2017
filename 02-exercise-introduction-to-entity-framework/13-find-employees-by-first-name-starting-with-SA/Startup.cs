93d5d01e45f691736b3a7b952af30b66826c0d52
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-01 22:34:52 +0200

    Add files via upload

---
+++ 02-exercise-introduction-to-entity-framework/13-find-employees-by-first-name-starting-with-SA/Startup.cs
@@ -0,0 +1,24 @@
+﻿namespace _13_find_employees_by_first_name_starting_with_SA
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            var softUniContext = new SoftUniContext();
+
+            //Filter employees
+
+            var employees = softUniContext.Employees
+                .Where(e => e.FirstName.Substring(0, 2) == "SA");
+
+            foreach (var employee in employees)
+            {
+                //I've updated the database twice at the previous task, so the results for Judge won't be correct
+
+                Console.WriteLine("{0} {1} - {2} (${3})", employee.FirstName, employee.LastName, employee.JobTitle, employee.Salary);
+            }
+        }
+    }
+}
