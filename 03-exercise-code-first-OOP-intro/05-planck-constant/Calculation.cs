20a7e14747f0af4e2e6ca6909ee40069aa176c32
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-11 12:09:34 +0200

    Add files via upload

---
+++ 03-exercise-code-first-OOP-intro/05-planck-constant/Calculation.cs
@@ -0,0 +1,13 @@
+﻿namespace _05_planck_constant
+{
+    public class Calculation
+    {
+        public const double PlanckConstant = 6.62606896e-34;
+        public const double Pi = 3.14159;
+
+        public static double reducedPlanck()
+        {
+            return PlanckConstant / (2 * Pi);
+        }
+    }
+}
---
+++ 03-exercise-code-first-OOP-intro/05-planck-constant/Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace _05_planck_constant
+{
+    using System;
+    class Startup
+    {
+        static void Main()
+        {
+            //test
+            Console.WriteLine(Calculation.reducedPlanck());
+        }
+    }
+}
