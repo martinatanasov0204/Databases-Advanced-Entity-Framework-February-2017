9bebab213f812f62409f7b0e6ebeadeab57cc6fd
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-11 12:08:30 +0200

    Add files via upload

---
+++ 03-exercise-code-first-OOP-intro/03-oldest-family-member/Family.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_oldest_family_member
+{
+    class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            members = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.members.Add(member);
+        }
+
+        public Person GetOldestPerson()
+        {
+            return this.members.OrderByDescending(m => m.Age).FirstOrDefault();
+        }
+    }
+}
---
+++ 03-exercise-code-first-OOP-intro/03-oldest-family-member/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace _03_oldest_family_member
+{
+    using System;
+    class Startup
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Family fam = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+                string name = line[0];
+                int age = int.Parse(line[1]);
+
+                fam.AddMember(new Person(name, age));
+            }
+
+            Person oldestOne = fam.GetOldestPerson();
+
+            Console.WriteLine("{0} {1}", oldestOne.Name, oldestOne.Age);
+        }
+    }
+}
