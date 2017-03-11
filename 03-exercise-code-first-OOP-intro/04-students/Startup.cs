e2e89a275e7f4b0ee977babf9ce0dc6fe348e750
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-11 12:09:04 +0200

    Add files via upload

---
+++ 03-exercise-code-first-OOP-intro/04-students/Startup.cs
@@ -0,0 +1,20 @@
+﻿namespace _04_students
+{
+    using System;
+    class Startup
+    {
+        static void Main()
+        {
+            var name = Console.ReadLine();
+            Students students = new Students(name);
+
+            while (name!="End")
+            {
+                name = Console.ReadLine();
+                students = new Students(name);
+            }
+
+            Console.WriteLine(Students.Count);
+        }
+    }
+}
---
+++ 03-exercise-code-first-OOP-intro/04-students/Students.cs
@@ -0,0 +1,14 @@
+﻿namespace _04_students
+{
+    class Students
+    {       
+        static private int Integer = 0;
+        static public int Count { get { return Integer; } }
+        public Students(string name)
+        {
+            this.Name = name;
+            Integer++;
+        }
+        public string Name { get; set; }
+    }
+}
