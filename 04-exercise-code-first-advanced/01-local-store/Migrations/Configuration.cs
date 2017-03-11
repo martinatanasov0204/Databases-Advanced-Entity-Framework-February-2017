39885d1d51396b2a508998be31dd7496ae3d6962
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-11 15:00:55 +0200

    Add files via upload

---
+++ 04-exercise-code-first-advanced/01-local-store/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace _01_local_store.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<_01_local_store.GickaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "_01_local_store.GickaContext";
+        }
+
+        protected override void Seed(_01_local_store.GickaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
