417d6d2952230d0b4346279686dcdc77c884c8f9
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-11 12:10:10 +0200

    Add files via upload

---
+++ 03-exercise-code-first-OOP-intro/06-math-utilities/MathUtil.cs
@@ -0,0 +1,26 @@
+﻿namespace _06_math_utilities
+{
+    public class MathUtil
+    {
+        public static double Sum(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public static double Subtract(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        public static double Multiply(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        public static double Divide (double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        public static double Percentage(double firstNumber, double secondNumber)
+        {
+            return 1.00 * firstNumber / 100 * secondNumber;
+        }
+    }
+}
---
+++ 03-exercise-code-first-OOP-intro/06-math-utilities/Startup.cs
@@ -0,0 +1,46 @@
+﻿namespace _06_math_utilities
+{
+    using System;
+    using System.Linq;
+    class Startup
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();                
+
+            while (input != "End")
+            {
+               var parts = input
+                    .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                var command = parts[0];
+                var firstNum = double.Parse(parts[1]);
+                var secondNum = double.Parse(parts[2]);
+
+                if (command == "Sum")
+                {
+                    Console.WriteLine("{0:F2}", MathUtil.Sum(firstNum, secondNum));
+                }
+                else if (command == "Subtract")
+                {
+                    Console.WriteLine("{0:F2}", MathUtil.Subtract(firstNum, secondNum));
+                }
+                else if (command == "Multiply")
+                {
+                    Console.WriteLine("{0:F2}", MathUtil.Multiply(firstNum, secondNum));
+                }
+                else if (command == "Divide")
+                {
+                    Console.WriteLine("{0:F2}", MathUtil.Divide(firstNum, secondNum));
+                }
+                else
+                {
+                    Console.WriteLine("{0:F2}", MathUtil.Percentage(firstNum, secondNum));
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
