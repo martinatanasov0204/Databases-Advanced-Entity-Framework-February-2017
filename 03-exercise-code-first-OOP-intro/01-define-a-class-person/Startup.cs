803c4802e54e86bdf5910c1d27b5bd9a3960656b
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-11 12:07:06 +0200

    Add files via upload

---
+++ 03-exercise-code-first-OOP-intro/01-define-a-class-person/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace _01_define_a_class_person
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Person person1 = new Person()
+            {
+                Name = "Pesho",
+                Age = 20
+            };
+
+            Person person2 = new Person()
+            {
+                Name = "Gosho",
+                Age = 18
+            };
+
+            Person person3 = new Person()
+            {
+                Name = "Stamat",
+                Age = 43
+            };
+        }
+    }
+}
