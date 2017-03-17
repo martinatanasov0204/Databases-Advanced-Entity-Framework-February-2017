0d07eda04014cc70ec79e4a6fefb4146c5b6c42b
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-17 17:00:08 +0200

    Add files via upload

---
+++ 05-exercise-entity-framework-relations/01-code-first-student-system - task 02/Models/Resources.cs
@@ -0,0 +1,21 @@
+﻿namespace _01_code_first_student_system.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Collections.Generic;
+    public class Resources
+    {
+        public Resources()
+        {
+            Cources = new HashSet<Course>();
+        }
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression("^video|presentation|document|other")]
+        public string TypeOfResource { get; set; }
+        [Required]
+        public string URL { get; set; }
+        public virtual ICollection<Course> Cources { get; set; }
+    }
+}
