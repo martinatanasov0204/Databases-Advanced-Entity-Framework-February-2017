c05c9e75b9ce53186f56f0e3fa42fc67abb41b6d
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-17 17:07:52 +0200

    Add files via upload

---
+++ 06-exercise-entity-relations-advanced/book-shop/Models/Author.cs
@@ -0,0 +1,15 @@
+﻿namespace _01_book_shop_system_DB.Models
+{
+    using System.Collections.Generic;
+    public class Author
+    {
+        public Author()
+        {
+            Books = new HashSet<Book>();
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
