b27c60f31877a3ed10df3fcb773377ad846e5155
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-17 16:54:33 +0200

    Add files via upload

---
+++ 05-exercise-entity-framework-relations/01-code-first-student-system/Models/Homework.cs
@@ -0,0 +1,17 @@
+﻿namespace _01_code_first_student_system.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Homework
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        [RegularExpression("^application|pdf|zip")]
+        public string ContentType { get; set; }
+        [Required]
+        public DateTime SubmissionDate { get; set; }
+        public Student Student { get; set; }
+    }
+}
---
+++ 05-exercise-entity-framework-relations/01-code-first-student-system/Startup.cs
@@ -0,0 +1,11 @@
+﻿namespace _01_code_first_student_system
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var context = new SchoolSystemContext();
+            context.Database.Initialize(true);
+        }
+    }
+}
