1ccd1a57385404097bd9413c5da4035a52facdb4
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-26 21:44:33 +0300

    Add files via upload

---
+++ 08-exercise-CS-auto-mapping-objects/03-projection/Classes/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03_projection.Classes
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            ManagerOf = new HashSet<Employee>();
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public Employee Manager { get; set; }
+        public virtual ICollection<Employee> ManagerOf { get; set; }
+    }
+}
---
+++ 08-exercise-CS-auto-mapping-objects/03-projection/Startup.cs
@@ -0,0 +1,62 @@
+﻿namespace _03_projection
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using AutoMapper;
+    using _03_projection.Classes;
+    class Startup
+    {
+        static void Main()
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<Employee, EmployeeDTO>());
+
+            var employees = new List<Employee>();
+
+            var employee = new Employee()
+            {
+                FirstName = "Pesho",
+                LastName = "Petrov",
+                Salary = 4000,
+                BirthDate = new DateTime(2005, 6, 7),
+                Manager = new Employee()
+                {
+                    FirstName = "ManagerName",
+                    LastName = "ManagerLastName"
+                }
+            };
+
+            var employee2 = new Employee()
+            {
+                FirstName = "Hesho",
+                LastName = "Heshev",
+                Salary = 5555,
+                BirthDate = new DateTime(1980, 10, 4),
+                Manager = new Employee()
+                {
+                    FirstName = "HeshoManName",
+                    LastName = "HeshoManLastName"
+                }
+            };
+
+            employees.Add(employee);
+            employees.Add(employee2);
+
+
+            foreach (var emp in employees.Where(e => e.BirthDate.Year < 1990).OrderByDescending(e => e.Salary))
+            {
+                var empDTO = Mapper.Map<EmployeeDTO>(emp);
+
+                Console.WriteLine($"{empDTO.FirstName} {empDTO.LastName} {empDTO.Salary} – Manager: {empDTO.ManagerLastName}");
+            }
+        }
+
+        public class EmployeeDTO
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public decimal Salary { get; set; }
+            public string ManagerLastName { get; set; }
+        }
+    }
+}
