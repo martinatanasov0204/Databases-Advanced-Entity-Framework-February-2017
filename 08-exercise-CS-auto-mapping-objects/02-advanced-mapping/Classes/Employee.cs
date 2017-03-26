607865d58e7a0377b68952cc585de4854fc0fbeb
martinatanasov0204 <martinatanasov0204@gmail.com>
2017-03-26 21:43:57 +0300

    Add files via upload

---
+++ 08-exercise-CS-auto-mapping-objects/02-advanced-mapping/Classes/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace _02_advanced_mapping.Classes
+{
+    using System;
+    using System.Collections.Generic;
+    public class Employee
+    {
+        public Employee()
+        {
+            ManagerOf = new HashSet<Employee>();
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Address { get; set; }
+        public bool OnHoliday { get; set; }
+        public Employee Manager { get; set; }
+        public virtual ICollection<Employee> ManagerOf { get; set; }       
+    }
+}
---
+++ 08-exercise-CS-auto-mapping-objects/02-advanced-mapping/Startup.cs
@@ -0,0 +1,54 @@
+﻿namespace _02_advanced_mapping
+{
+    using System;
+    using System.Collections.Generic;
+    using AutoMapper;
+    using _02_advanced_mapping.Classes;
+    class Startup
+    {
+        static void Main()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Employee, EmployeeDTO>();
+                cfg.CreateMap<Employee, ManagerDTO>();
+            });
+
+            var employee = new Employee()
+            {
+                FirstName = "Pesho",
+                LastName = "Petrov",
+                Salary = 4000,
+                Manager = new Employee()
+                {
+                    FirstName = "ManagerName",
+                    LastName = "ManagerLastName"
+                }
+            };
+
+            employee.Manager.ManagerOf.Add(new Employee()
+            {
+                FirstName = "Emp1",
+                LastName = "EmpLastName1",
+                Salary = 5431
+            });
+
+            ManagerDTO manager = Mapper.Map<ManagerDTO>(employee.Manager);
+
+            Console.WriteLine($"{manager.FirstName} {manager.LastName} | Employees: {manager.ManagerOf.Count}");
+        }
+
+        public class EmployeeDTO
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public decimal Salary { get; set; }
+        }
+        public class ManagerDTO
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public List<EmployeeDTO> ManagerOf { get; set; }
+        }
+    }
+}
